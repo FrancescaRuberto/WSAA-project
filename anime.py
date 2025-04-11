25f46483a87847ab9bd2547de7fea3b51f36033b
Francesca Ruberto <francescaruberto01@gmail.com>
2025-04-11 19:15:14 +0100

    created dao file

---
+++ anime.py
@@ -0,0 +1,17 @@
+# anime.py (basic version)
+
+# CRUD operations
+def getall():
+    return [{}]  
+
+def find_by_id(id):
+    return {}  
+
+def create(anime):
+    return anime
+
+def update(id, anime):
+    return anime 
+
+def delete(id):
+    return True
